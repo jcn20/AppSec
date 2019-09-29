debbc145f12c86cf49d6a194d0cf5372f4ad689f
jcn20 <julionunez96@gmail.com>
2019-09-29 19:24:55 -0400

    Changed for loop to test num_misspelled.

--- spell.c
+++ spell.c
@@ -148,7 +148,7 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]){
             num_misspelled++;
             }
         }
-    for(int i=0; i < MAX_MISSPELLED; i++){
+    for(int i = 0; i < MAX_MISSPELLED; i++){
         printf("%d\n", misspelled[MAX_MISSPELLED][i]);
     }
     return num_misspelled;
